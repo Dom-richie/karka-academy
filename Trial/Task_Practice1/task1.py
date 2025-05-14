f59a1966ec8de6e3a69294e85acf57797fb904ad
Dom-Richie <dom883407@gmail.com>
2025-05-13 23:22:30 -0700

    task1

---
+++ Trial/Task_Practice1/task1.py
@@ -0,0 +1,66 @@
+#1
+print("hello, World!")
+#2
+print("My Name Is Dominic Richard")
+#3
+print("My Name Is Dominic Richard")
+print("My Age Is 24")
+#4
+name=("richard") 
+print(name)
+age=24
+print(age)
+is_student=True
+is_married=False
+print(is_student)
+print(is_married)
+#5
+print("My Name Is-",name,"My Age Is-",age)
+#6
+name=("richard") 
+age=24
+print(name,age)
+#7
+a=10
+b=5
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print(c)
+print(d)
+print(e)
+print(f)
+#8
+Friends=[f"john,richard,santhosh,zen"]
+print(Friends)
+#9
+print("*")
+print("**")
+print("***")
+print("****")
+#10
+name="richard"
+age=24
+isStudent=True
+print(name,age,isStudent)
+#11
+name="john"
+age+=1
+isStudent!=True
+name="richard"
+age=24
+dob="13-10-2001"
+height=5.10
+weight=66
+degree="Bachelor's in computer science"
+gender="male"
+is_alive=True
+print("name:",name)
+print("age",age)
+print("DOB:",dob)
+print("HEIGHT:",height)
+print("weight:",weight)
+print("Degree:",degree)
+print("gender:",gender)
+print("is_alive:",is_alive)
