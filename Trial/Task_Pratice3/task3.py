d39ece3af8650e65317b5b121951a3f4aac4fc86
Dom-richie <dom883407@gmail.com>
2025-05-14 02:51:09 -0700

    task3

---
+++ Trial/Task_Pratice3/task3.py
@@ -0,0 +1,36 @@
+
+#1
+def sayhello(b):
+    a=b
+    print(a)
+sayhello("Hello,World!")
+#2
+def add(a,b):
+    c=a+b
+    print(c)
+add(10,15)
+#3
+def multiply(a,b):
+    c=a*b
+    print(c)
+multiply(10,15)
+#4
+def multiply(a,b):
+    c=a*b
+    print(c)
+    return(c)
+multiply(10,15)
+#6
+n=5
+num=1
+for i in range (1,n+1):
+    num*=i
+print(num)
+#7
+def square(a):
+    result=a*a
+    print(result)
+square(5)
+square(10)
+square(2)
+square(6)
